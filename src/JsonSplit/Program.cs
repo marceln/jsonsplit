5a9ade1c9d39d137bf49b6f0a3aa15c4443dbfcc
Marcel Nita <marcel.nita@gmail.com>
2020-01-19 23:21:21 +0200

    Start mmpoc

---
+++ src/JsonSplit/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+
+namespace JsonSplit
+{
+    public class Program
+    {
+        private static string JSONTEXT = @"[{""a"":0},{}, ]";
+
+        static void Main(string[] args)
+        {
+            ReadJson();
+        }
+
+        private static void ReadJson() 
+        {
+            var options = new JsonReaderOptions
+            {
+                AllowTrailingCommas = true,
+                CommentHandling = JsonCommentHandling.Skip
+            };
+            var jsonBytes = Encoding.UTF8.GetBytes(JSONTEXT);
+            Utf8JsonReader reader = new Utf8JsonReader(jsonBytes, options);
+
+            while (reader.Read())
+            {
+                Console.Write(reader.TokenType);
+
+                // switch (reader.TokenType)
+                // {
+                //     case JsonTokenType.PropertyName:
+                //     case JsonTokenType.String:
+                //         {
+                //             string text = reader.GetString();
+                //             Console.Write(" ");
+                //             Console.Write(text);
+                //             break;
+                //         }
+
+                //     case JsonTokenType.Number:
+                //         {
+                //             int intValue = reader.GetInt32();
+                //             Console.Write(" ");
+                //             Console.Write(intValue);
+                //             break;
+                //         }
+
+                //         // Other token types elided for brevity
+                // }
+                Console.WriteLine();
+            }
+        }
+    }
+}
